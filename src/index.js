58f80a22b8403975b74d7a995e4dab43efd1bc87
thecoderme <yasin.badur.nk@gmail.com>
2024-06-07 12:03:57 +0300

    v1

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import VirtualTryOnGlasses from "./VirtualTryOnGlasses";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <VirtualTryOnGlasses />
+  </React.StrictMode>
+);
